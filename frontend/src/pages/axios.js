f725582b46a0f13e636af4a61d0828ca679ee602
dev-vikash2v7 <vikashvermacom92@gmail.com>
2025-08-07 13:01:44 +0530

    'update'

--- frontend/src/pages/axios.js
+++ frontend/src/pages/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://reacttubevideos.firebaseapp.com',
+  baseURL: 'https://warehouse-management-backend.vercel.app',
 
 });
 
